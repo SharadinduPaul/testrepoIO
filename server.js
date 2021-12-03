92bd8570fb6544f070ea4c10f6efd7c9a7ada7aa
Sayan Paul <sharadindu.paul007@gmail.com>
2021-12-03 07:36:38 +0530

    last commmit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 io.on("connection", (socket) => {
   socket.emit("msg", "welcome to first socket io app");
